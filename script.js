547b163a36e018d44abba1b2caccb0cc775136e4
shukant75 <spanjikar9155@gmail.com>
2025-05-07 05:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
   // your code here
+	// Trim leading spaces
+  s = s.trimStart();
+
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If no space found, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring up to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
